37c62657dcfb64ec1cf4d1627b34ce3e1d8ddac2
sarden0302 <sarden030@gmail.com>
2024-12-11 11:47:11 +0900

    String / StringBuilder/ StringBuffer/ Thread/ Process

---
+++ JAVAPROJECT/src/com/kh/stringEx/StringBufferEx.java
@@ -0,0 +1,4 @@
+package com.kh.stringEx;
+
+public class StringBufferEx {
+}
--- JAVAPROJECT/src/com/kh/testEx/test3pre/StudentScoreManager.java
+++ JAVAPROJECT/src/com/kh/testEx/test3pre/StudentScoreManager.java
@@ -1,10 +1,89 @@
 package com.kh.testEx.test3pre;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class StudentScoreManager {
+    private static Set<String> studentSet = new HashSet<String>();
+    static Scanner sc = new Scanner(System.in);
+    public static void setMainMenu() {
+        boolean exit = false;
+        while (!exit) {
+            System.out.println("학생 관리 프로그램");
+            System.out.println("1. 학생 추가");
+            System.out.println("2. 학생 삭제");
+            System.out.println("3. 학생 목록 출력");
+            System.out.println("4. 특정 학생 존재 여부 확인");
+            System.out.println("5. 종료");
+            System.out.print("메뉴 선택: ");
+            try {
+                int choice = Integer.parseInt(sc.nextLine());
+                switch (choice) {
+                    case 1:
+                        addingStudent();
+                        break;
+                    case 2:
+                        deleteStudent();
+                        break;
+                    case 3:
+                        showingAll();
+                        break;
+                    case 4:
+                        isExist();
+                        break;
+                    case 5:
+                        exit = true;
+                        return;
+                    default:
+                        System.out.println("1, 2, 3, 4, 5 중에 입력해주세요.");
+                }
+
+            } catch (NumberFormatException e) {
+                System.out.println("잘못 선택하셨습니다 메인 화면으로 돌아갑니다.");
+            }
+        }
+    }
+
+    public static void isExist() {
+        if (studentSet.isEmpty()) {
+            System.out.println("현재 학생 명단은 비어있습니다.");
+            return;
+        }
+
+        System.out.print("확인할 학생의 이름을 입력해주세요.");
+        System.out.println( (studentSet.contains(sc.nextLine())) ? "존재합니다." : "존재하지 않습니다.");
+    }
+
+    public static void showingAll() {
+        if (studentSet.isEmpty()) {
+            System.out.println("현재 학생의 명단은 비어있습니다.");
+            return;
+        }
+
+        for (String student : studentSet) {
+            System.out.println(student);
+        }
+    }
+
+    public static void addingStudent() {
+        System.out.print("이름을 입력해주세요 : ");
+        studentSet.add(sc.nextLine());
+    }
+
+    public static void deleteStudent() {
+        if (studentSet.isEmpty()) {
+            System.out.println("현재 학생 명단은 비어있습니다.");
+            return ;
+        }
+        System.out.print("삭제할 학생의 이름을 입력해주세요 : ");
+        String name = sc.nextLine();
+        if (studentSet.contains(name)) {
+            studentSet.remove(name);
+            System.out.println(name + "학생은 삭제되었습니다.");
+            return;
+        }
+        System.out.println("해당 학생은 명단에 없습니다. 메인화면으로 돌아갑니다.");
+    }
+
     // 점수 출력 기능 메서드
     public static void checkingPassNoPass(Map<String, Integer> student) {
         System.out.println("---- Pass / NoPass ----");
@@ -17,6 +96,7 @@ public class StudentScoreManager {
         }
     }
     public static void main(String[] args) {
+        /*
         Map<String, Integer> student = new HashMap<String, Integer>();
         Scanner scanner = new Scanner(System.in);
 
@@ -47,6 +127,8 @@ public class StudentScoreManager {
         } catch (NumberFormatException e) {
             System.out.println("잘못 입력하였습니다.");
         }
+       */
+        setMainMenu();
 
     }
 }
